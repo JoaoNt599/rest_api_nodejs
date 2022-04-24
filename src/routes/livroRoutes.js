3016c53bd21dc2deccf8353cac975386f54f1194
João Neto <netojoao599@gmail.com>
2022-04-24 00:20:20 -0300

    adicionando metodos get post put e delete

--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -4,6 +4,10 @@ import LivroController from "../controllers/livrosController.js";
 const router = express.Router();
 
 router
-    .get("/livros",LivroController.listarLivros)
+    .get("/livros", LivroController.listarLivros)
+    .get("/livros/:id", LivroController.listarLivroPorID)
+    .post("/livros", LivroController.cadastraLivro)
+    .put("/livros/:id", LivroController.atulaizarLivro)
+    .delete("/livros/:id", LivroController.excluirLivro)
 
 export default router;
